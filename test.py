55feddbd99cf70b61f4f4da4691dfcdea2a0e090
A3402 <A3402@lab.aca>
2019-02-20 18:46:35 -0500

    test.py

--- test.py
+++ test.py
@@ -2,7 +2,10 @@ import unittest
 from game import Juego
 
 class testJuego(unittest.TestCase):
-    pass
+    def testnombreVentana(self):
+        juego=Juego()
+        nombre_ventana=juego.set_nombre_ventana
+        self.assertEqual(nombre_ventana,"Prueba de jugabilidad")
     
 
 if __name__ == '__main__':
